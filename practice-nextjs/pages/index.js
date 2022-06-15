101ff2036fd97cb7d6aff96c0ed3d186655fd076
free-ko <duddnr654@naver.com>
2022-06-15 21:48:42 +0900

    [Study] : getServerSideProps 설명 추가

--- practice-nextjs/pages/index.js
+++ practice-nextjs/pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ results }) {
       <Seo title="Home" />
       {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -18,6 +18,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -49,3 +52,27 @@ export async function getServerSideProps() {
     },
   };
 }
+
+/**
+ * getServerSideProps 추가 설명
+https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+
+Q) 언제 getServerSideProps를 사용해야 하나요?
+request time에 반드시 데이터를 fetch해와야 하는 페이지를 pre-render해야 하는 경우에만 getServerSideProps를 사용해야 합니다.
+데이터를 pre-render할 필요가 없다면 client side에서 데이터를 가져오는 것을 고려해야 합니다.
+
+클라이언트 측에서 데이터 가져오는 과정 (Fetching data on the client side)
+페이지에 자주 업데이트되는 데이터가 포함되어 있고 데이터를 pre-render할 필요가 없는 경우 클라이언트 측에서 데이터를 가져올 수 있습니다.
+1. 먼저 데이터가 없는 페이지를 즉시 표시합니다.
+2. 페이지의 일부는 Static Generation을 사용해 pre-render할 수 있습니다.
+3. 없는 데이터를 위해 loading 상태를 표시할 수 있습니다.
+4. 그런 다음 클라이언트 측에서 데이터를 가져와 준비가 되면 표시합니다.
+
+이 접근 방식은 예를 들어 사용자 대시보드 페이지에 적합합니다.
+왜냐하면 대시보드는 사용자별 비공개 페이지이기 때문에 SEO와는 관련이 없으며 페이지를 미리 렌더링할 필요가 없습니다. 또한 데이터는 자주 업데이트되므로 요청 시 데이터를 가져와야 합니다.
+
+getServerSideProps가 오류 페이지를 렌더링합니까?
+getServerSideProps 내부에서 오류가 발생하면 pages/500.js 파일이 표시됩니다.
+500 page(서버 렌더링 오류 페이지)는 사용자가 커스터 마이징 할 수 있습니다.
+개발 중에는 이 파일이 사용되지 않고 대신 개발 오버레이가 표시됩니다.
+ */
